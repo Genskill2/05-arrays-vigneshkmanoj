62c180ebc02bc34bd13cd42de74836aa24ce1f7c
vigneshkmanoj <84789258+vigneshkmanoj@users.noreply.github.com>
2021-06-14 16:38:22 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,14 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int z[],int count)
+{
+  count=sizeof(z);
+  for(int i=0;i<count;i++)
+  {
+    if(z[0]<z[i+1])
+    {
+      z[0]=z[i];
+    }
+  }
+  printf("%d",z[0]);
+}
