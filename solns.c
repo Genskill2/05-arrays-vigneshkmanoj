5ba1932c4c6908e196b36ef0eb6a9782f4423074
vigneshkmanoj <84789258+vigneshkmanoj@users.noreply.github.com>
2021-06-14 17:25:51 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,10 +1,20 @@
 /* Enter your solutions in this file */
-#include <stdio.h>
 int max(int z[],int count)
 {
   for(int i=0;i<count;++i)
   {
     if(z[0]<z[i])
+    {
+      z[0]=z[i];
+    }
+  }
+  return z[0];
+}
+int min(int z[],int count)
+{
+  for(int i=0;i<count;++i)
+  {
+    if(z[0]>z[i])
     {
       z[0]=z[i];
     }
