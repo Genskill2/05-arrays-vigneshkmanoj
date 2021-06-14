5578f80c0e39a1806d22d17e0b91be0df4b079b3
vigneshkmanoj <84789258+vigneshkmanoj@users.noreply.github.com>
2021-06-14 19:19:19 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,5 +1,6 @@
 /* Enter your solutions in this file */
-int max(int z[],int count)
+#include<stdio.h>
+ int max(int z[],int count)
 {
   for(int i=0;i<count;++i)
   {
@@ -10,7 +11,7 @@ int max(int z[],int count)
   }
   return z[0];
 }
-int min(int z[],int count)
+  int min(int z[],int count)
 {
   for(int i=0;i<count;++i)
   {
@@ -21,3 +22,31 @@ int min(int z[],int count)
   }
   return z[0];
 }
+  float average(int z[],int count)
+  {
+  float sum=0;
+  for(int i=0;i<count;i++)
+  {
+  sum=sum+z[i];
+  }
+  return sum/count;
+  }
+  int mode(int z[],int n)
+  {
+  int value=0,max_count=0;
+  for(int i=0;i<n;i++)
+  {
+  int count=0;
+  for(int j=0;j<n;j++)
+  {
+  if(z[i]==z[j])
+  count=count+1;
+  }
+  if(count>max_count)
+  {
+  max_count=count;
+  value=z[i];
+  }
+  }
+  return value;
+  }
